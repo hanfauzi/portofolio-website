a77708bbf4e895955dec06a24a7cc299c7c6fbd1
hanfauzi <nifauzi45@gmail.com>
2025-07-04 13:58:33 +0700

    perbiakan next

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,20 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    optimizeCss: true,
+  },
   images: {
     domains: [
-      "design4users.com", // ✅ finance dashboard
-      "clarylifeglobal.com", // ✅ real estate platform
-      "preview.redd.it", // ✅ resume builder
-      "miro.medium.com", // ✅ LMS for EduPro
-      "encrypted-tbn0.gstatic.com", 
+      "design4users.com",        // ✅ finance dashboard
+      "clarylifeglobal.com",     // ✅ real estate platform
+      "preview.redd.it",         // ✅ resume builder
+      "miro.medium.com",         // ✅ LMS for EduPro
+      "encrypted-tbn0.gstatic.com",
       "mir-s3-cdn-cf.behance.net",
-      "images.ctfassets.net"// ✅ restaurant booking & analytics dashboard (sama domain)
+      "images.ctfassets.net"     // ✅ restaurant booking & analytics dashboard
     ],
   },
 };
+
 export default nextConfig;
